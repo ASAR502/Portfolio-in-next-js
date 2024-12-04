cb99676afc279b45bd1a51bd5aecf0de80859f93
Aaquib <aaquibm502@gmail.com>
2024-12-04 14:46:43 +0530

    correction after deployment

--- src/constants/data/text.jsx
+++ src/constants/data/text.jsx
@@ -6,19 +6,19 @@ export const aboutText1 =
   "Intrigued with web technologies. I love developing creative and user friendly interfaces. I can build memorable web experience for your end-users driving profit to your business";
 
 export const aboutText2 =
-  "Hi there! I'm Aaquib, a passionate Software Developer with 4+ years of experience. I specialize in helping startups achieve their goals by building products and experiences that are intuitive, user-centric, and optimized for high conversion rates";
+  "Hi there! I'm Aaquib, a passionate Software Developer with better experience. I specialize in helping startups achieve their goals by building products and experiences that are intuitive, user-centric, and optimized for high conversion rates";
 export const ctatext5 =
-  "I am Aaquib Shahzada and I am a self-learned software developer, dedicated in creating appealing user-interfaces for industries since 2020";
+  "I am Aaquib Shahzada and I am a self-learned software developer, dedicated in creating appealing user-interfaces for industries since 2022";
 
 export const ctatext1 =
   "When I'm not building or exploring new web experiences, I'm probably playing games or watching football.";
-export const ctatext3 = `I am your guy to go to! I can work and communicate effectively with design focused teams to build web applications. I have years of experience collaborating with product teams.`;
+export const ctatext3 = `I am your guy to go to! I can work and communicate effectively with design focused teams to build web applications. I have a  experience  to collaborating with product teams.`;
 export const ctatext6 =
   "What sets me apart is my belief in the power of collaboration and mentorship. I strongly believe that empowered teams build great products. Whether I’m creating dynamic front-end interfaces or architecting backend systems, I bring creativity, precision, and an unwavering focus on delivering impactful results.";
 export const ctatext7 =
-  "I'm currently open to new opportunities, particularly where I can make a meaningful difference. Let’s connect and discuss how I can contribute to your team! Feel free to reach me at victoressienscript@gmail.com.";
+  "I'm currently open to new opportunities, particularly where I can make a meaningful difference. Let’s connect and discuss how I can contribute to your team! Feel free to reach me aaquibm502@gmail.com.";
   export const ctatext4 =
-  "Throughout my career, I’ve honed my skills in crafting scalable and user-friendly solutions using React, Go, Node.js, Next.js, TypeScript, and Svelte. My passion for learning has also driven me to explore DevOps, leveraging tools like GitHub Actions and Jenkins to streamline CI/CD pipelines for large-scale enterprise solutions.";
+  "Throughout my career, I’ve honed my skills in crafting scalable and user-friendly solutions using React, Node.js, Next.js, TypeScript. My passion for learning has also driven me to explore DevOps, leveraging tools like GitHub Actions Enso .";
 
 export const skillslist = [
   {
@@ -81,6 +81,11 @@ export const skillslist = [
     role: "States Management",
     title: "REDUX",
     icons: "/icons/redux.png",
+  },
+  {
+    role: "States Management",
+    title: "RCOIL",
+    icons: "/icons/recoil.png",
   },
   {
     role: "Styling frontend application",
@@ -168,34 +173,19 @@ export const serviceslist = [
 
 export const experienceList = [
   {
+    title: "Enso Innovation Lab",
+    position: "Full Stack Devloper",
+    date: "  Jan 2024 - continue",
+    role: "Devopl various user interaction website therapist portal ",
+    location: " New Delhi",
+  },
+  {
     title: "OrcaBills",
     position: "Full-Stack Engineer",
     date: "April 2022 - Aug 2023 ",
     role: " Bridged the gap between frontend and backend, delivering a dynamic and personalized userexperience, tailored to each user and organization, resulting in a 25% increase in user satisfaction.",
     location: "New Delhi",
-  },
-  {
-    title: "Enso Innovation Lab",
-    position: "Full Stack Devloper",
-    date: "  May 2024 - continue",
-    role: "Devopl various user interaction website therapist portal ",
-    location: " New Delhi",
-  },
-  // {
-  //   title: "Palkeeper",
-  //   position: "Frontend Engineer",
-  //   date: "August 2020 - May 2021",
-  //   role: " Coordinated with backend developers to align API requirements and data formats, reducing integration issues by 30% and ensuring a seamless user experience",
-  //   location: " United States",
-  // },
-
-  // {
-  //   title: "Trevlo Technologies Limited",
-  //   position: "Frontend Intern",
-  //   date: "February 2019 - March 2020",
-  //   role: "Learn and analyze design mockups to understand layout, color schemes, typography, and interactive elements, contributing to a 25% improvement in design implementation accuracy",
-  //   location: "Imo State, Nigeria",
-  // },
+  }
 ];
 
 export const skillDescriptionText =
